73905a2a453665b7628af43e33b3d78d53e9b890
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2023-11-18 13:03:34 +0530

    practice

---
+++ Interface/Main.java
@@ -0,0 +1,23 @@
+package Interface;
+public class Main {
+    public static void main(String[] args) {
+        Circle c = new Circle(5);
+        System.out.println(c.calculateArea());
+    }
+}
+
+interface Shape{
+    double calculateArea();
+}
+
+class Circle implements Shape{
+    
+    double radius;
+    Circle(double radius){
+        this.radius = radius;
+    }
+    
+    public double calculateArea(){
+        return Math.PI*radius*radius;
+    }
+}
