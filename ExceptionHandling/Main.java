af7449e80f59171dc0383a3a809d655cfcd4e859
Shubham Dhatwalia <shubhamdhatwalia3@gmail.com>
2023-11-18 15:40:08 +0530

    practice

--- ExceptionHandling/Main.java
+++ ExceptionHandling/Main.java
@@ -1,22 +1,26 @@
-// package ExceptionHandling;
+// Exception handling is a programming mechanism that deals with runtime errors or exceptional situations that may occur during the execution of a program. These errors are typically beyond the control of the programmer and may disrupt the normal flow of a program. Java provides a robust system for exception handling with keywords such as try, catch, finally, throw, and throws.
 
-// public class Main {
-//     public static void main(String[] args) {
-        
-//         int arr[] = {1,2,3,4,5,6,7,8};
 
-//         System.out.println(arr[1]);
-//         try{
-//         System.out.println(arr[-1]);
-//         }
-//         catch(Exception e){
-//             System.out.println(e.getMessage());
-//         }
-//         finally{
-//         System.out.println(arr[5]);
-//         }
-//     }
-// }
+// Try:
+
+// The try block is used to enclose a section of code where an exception might occur. It is followed by one or more catch blocks and an optional finally block.
+
+// Catch:
+
+// The catch block is used to catch and handle exceptions thrown in the corresponding try block. Multiple catch blocks can be used to handle different types of exceptions.
+
+// Finally:
+
+// The finally block is optional and is used to specify code that must be executed, regardless of whether an exception occurred or not. It is often used for cleanup operations (e.g., closing resources).
+
+// Throw:
+
+// The throw keyword is used to explicitly throw an exception. You can throw a specific exception or create your own custom exception.
+
+// Throws:
+
+// The throws clause is used in a method signature to declare that a method may throw one or more types of exceptions. It informs the caller of the method about the exceptions it might need to handle.
+
 
 package ExceptionHandling;
 
